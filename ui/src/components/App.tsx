bc7b67267a653c0f8eef43d4fabdc519193e90fd
marvybaby <mbamalumarvellous@gmail.com>
2026-07-12 09:20:00 +0100

    Add RoleContext for role-based dashboard routing

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -5,21 +5,30 @@ import { createLedgerContext } from "@daml/react";
 import { Credentials } from "../Credentials";
 
 export const userContext = createLedgerContext();
+export const RoleContext = React.createContext<string>('');
 
 const App: React.FC = () => {
   const [credentials, setCredentials] = React.useState<Credentials | undefined>(undefined);
+  const [role, setRole] = React.useState<string>('');
+
+  const handleLogin = (creds: Credentials, selectedRole: string) => {
+    setCredentials(creds);
+    setRole(selectedRole);
+  };
 
   if (credentials) {
     return (
-      <userContext.DamlLedger
-        token={credentials.token}
-        party={credentials.party}
-        user={credentials.user}>
-        <MainScreen onLogout={() => setCredentials(undefined)} />
-      </userContext.DamlLedger>
+      <RoleContext.Provider value={role}>
+        <userContext.DamlLedger
+          token={credentials.token}
+          party={credentials.party}
+          user={credentials.user}>
+          <MainScreen onLogout={() => { setCredentials(undefined); setRole(''); }} />
+        </userContext.DamlLedger>
+      </RoleContext.Provider>
     );
   } else {
-    return <LoginScreen onLogin={setCredentials} />;
+    return <LoginScreen onLogin={handleLogin} />;
   }
 };
 
