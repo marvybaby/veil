b4e96cc26aa10789a0f699e1e4630537e1623e7d
marvybaby <mbamalumarvellous@gmail.com>
2026-07-12 09:22:10 +0100

    Add Window type declarations for Loop SDK globals

--- ui/src/react-app-env.d.ts
+++ ui/src/react-app-env.d.ts
@@ -1,4 +1,9 @@
-// Copyright (c) 2022 Digital Asset (Switzerland) GmbH and/or its affiliates. All rights reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 /// <reference types="react-scripts" />
+
+interface Window {
+  __loopSdk: any;
+  __veilAuth: {
+    partyId: string;
+    provider: any;
+  } | undefined;
+}
